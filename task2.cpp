b9387c6a90f7224153909867033973a5386b7faf
BalaDipu <43338119+BalaDipu@users.noreply.github.com>
2019-07-14 14:11:25 +0600

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,43 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+vector<string> tokens;
+int value[1000];
+int base_value(int base, string s)
+{
+    int base_sum = 0;
+    int rad = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        rad = ((int)s[i]) * pow(base, i);
+        base_sum = (base_sum + rad) % 997;
+    }
+    return base_sum;
+}
+void stringTokenize(string line)
+{
+    stringstream check1(line);
+    string intermediate;
+    while (getline(check1, intermediate, ' '))
+    {
+        tokens.push_back(intermediate);
+    }
+}
+int main()
+{
+    int base = 26;
+    string s;
+    getline(cin, s);
+    stringTokenize(s);
+    for (int i = 0; i < tokens.size(); i++)
+    {
+        int base_sum = base_value(base, tokens[i]);
+        value[base_sum]++;
+    }
+
+    for(int i=0; i<tokens.size(); i++)
+    {
+        int base_sum = base_value(base, tokens[i]);
+        cout << tokens[i] << ' ' << value[base_sum] << endl;
+    }
+	   }
